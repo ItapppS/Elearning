b8cba901a85accdc061f6015dcd65cb99125350e
uttamgiri1316 <uttamgiri131689@gmail.com>
2025-02-27 11:49:09 +0530

    add register page and changes in database

--- Eapp/models.py
+++ Eapp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class PhnUser(models.Model):
-
-    first_name = models.CharField(max_length=50,default=True)
-    last_name = models.CharField(max_length=50,default=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     gmail = models.EmailField(unique=True)
-    age = models.PositiveIntegerField()
+    password = models.CharField(max_length=128, default=True)  # ✅ Required Password
+    age = models.PositiveIntegerField(default=True)
     gender = models.CharField(max_length=10, default=True)
-    address = models.TextField(max_length=50,default=True)
+    address = models.TextField(max_length=255, default=True)
     mobile_no = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
