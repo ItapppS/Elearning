b9da2ea4830c41dbbd62b9f07688bc58bb14c3fe
VasimShaikh1710 <vasimmrshaikh@gmil.com>
2025-02-27 14:36:33 +0530

    update in base.html

--- Eapp/views.py
+++ Eapp/views.py
@@ -138,9 +138,6 @@ def register(request):
     return render(request, 'register.html')
 
 
-    
-    return render(request, 'register.html', {'form': form})
-
 
 def login(request):
     return render(request, 'login.html')
